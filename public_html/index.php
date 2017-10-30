1584bb40b22528f2eb94aae295fa03c112ed8e16
kobancheg <aleks.revati@yandex.ru>
2017-10-30 20:12:26 +0300

    test

--- public_html/index.php
+++ public_html/index.php
@@ -4,8 +4,8 @@ $APPLICATION->SetPageProperty("title", "Интернет-магазин Удив
 $APPLICATION->SetPageProperty("keywords", "полезные изобретения, уникальные товары, необычный садовый инструмент, товары для здоровья");
 $APPLICATION->SetPageProperty("description", "Интернет-магазин Удивительные Вещи - уникальные товары, удивительные товары, полезные изобретения, товары на видео");
 $APPLICATION->SetTitle("Интернет-магазин «Удивительные Вещи»");?>
-    <span style="font-size: 18px;">
+    <!--<span style="font-size: 18px;">
         <span class="imp"> Уважаемые покупатели!</span>
     До 15 сентября 2017 г. заказы по телефону не принимаются. Пользуйтесь системой оформления заказов через сайт. Доставка заказов осуществляется в обычном режиме.
-    </span>
+    </span>-->
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
